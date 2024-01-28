10059a1ac94027613c0e84a362afcd3c400b0ca8
Przemyslaw Rychlewicz <przemyslaw.rychlewicz@mettle.co.uk>
2024-01-28 14:12:13 +0000

    misc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from "@mui/joy"
+import { Box, Button, Sheet, Stack, Typography } from "@mui/joy"
 import { type HeadFC, type PageProps } from "gatsby"
 import * as React from "react"
 import { useConfigContext } from "../contexts"
@@ -10,14 +10,27 @@ const IndexPage: React.FC<PageProps> = () => {
     <Stack
       width="100%"
       height="100%"
-      spacing={1}
+      spacing={2}
       direction="row"
       justifyContent="center"
       alignItems="center"
     >
-      <Button color="danger" onClick={() => null} size="lg" variant="solid">
-        {translations.downloadButton}
-      </Button>
+      <Sheet component={Stack} height="100%" width="100%"></Sheet>
+
+      <Stack width="100%" height="100%" alignItems="flex-start" spacing={2}>
+        <Typography level="h1">AAA</Typography>
+
+        <Typography level="body-md">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
+          laoreet lacus, et mollis lectus. Quisque mattis pulvinar sem a
+          vestibulum. Quisque pulvinar justo massa, sed lacinia ex vestibulum
+          eu. Cras in massa tellus. Nullam eros dolor, dictum id dolor ornare,
+          feugiat efficitur arcu.
+        </Typography>
+        <Button color="danger" onClick={() => null} size="lg" variant="solid">
+          {translations.downloadButton}
+        </Button>
+      </Stack>
     </Stack>
   )
 }
